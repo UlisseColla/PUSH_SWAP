f775377b64596dbccf60af420e0f6f34494900c3
Andrea Conciarelli <aconciar@c2r5p7.42firenze.it>
2024-01-30 15:56:28 +0100

    ok

--- aconciar/push_swap_utils/ft_print_stack.c
+++ aconciar/push_swap_utils/ft_print_stack.c
@@ -6,7 +6,7 @@
 /*   By: aconciar <aconciar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 18:00:43 by aconciar          #+#    #+#             */
-/*   Updated: 2024/01/24 19:06:21 by aconciar         ###   ########.fr       */
+/*   Updated: 2024/01/29 16:18:02 by aconciar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	ft_print_stack(t_stack *stack)
 {
 	while(stack)
 	{
-		ft_printf("%d\n", stack->index);
+		if (stack->push == 1)
+			ft_printf("%d\n", stack->index);
 		stack = stack->next;
 	}
 }
